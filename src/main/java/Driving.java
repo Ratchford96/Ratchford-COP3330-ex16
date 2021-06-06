9a2dd940001fde136440b4976836a8d78a35e974
Ratchford96 <84363296+Ratchford96@users.noreply.github.com>
2021-06-06 15:11:44 -0400

    Initial commit

---
+++ src/main/java/Driving.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Driving {
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        int age;
+        // This print how old the person is in the program
+        System.out.print(" What is your age? ");
+        age = scan.nextInt();
+
+
+        // tells the user that if you are younger than 16 you can't legally drive
+        if(age < 16)
+        {
+            System.out.println(" You are not old enough to legally drive.");
+        }
+        else
+        {
+            System.out.println(" You are old enough to legally drive.");
+        }
+
+
+    }
+}
